78ae5a3fe6ff81058b949a3d117a396389361871
HeechanYang <yhc944@gmail.com>
2019-05-14 01:24:24 +0900

    [DELETE] Configuration

--- src/main/java/rabbitmq/util/CustomConnection.java
+++ src/main/java/rabbitmq/util/CustomConnection.java
@@ -2,18 +2,16 @@ package rabbitmq.util;
 
 import com.rabbitmq.client.ConnectionFactory;
 
+import static rabbitmq.util.Config.*;
+
 public class CustomConnection {
-    private static String MQTT_HOST = "52.79.158.171";
-    private static String MQTT_USERNAME = "user1";
-    private static String MQTT_PASSWORD = "test";
-    private static String MQTT_VIRTUAL_HOST = "dev1";
 
     public static ConnectionFactory getConnectionFactory(){
         ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost(MQTT_HOST);
-        factory.setUsername(MQTT_USERNAME);
-        factory.setPassword(MQTT_PASSWORD);
-        factory.setVirtualHost(MQTT_VIRTUAL_HOST);
+        factory.setHost(Config.MQTT_HOST);
+        factory.setUsername(Config.MQTT_USERNAME);
+        factory.setPassword(Config.MQTT_PASSWORD);
+        factory.setVirtualHost(Config.MQTT_VIRTUAL_HOST);
         return factory;
     }
 }
